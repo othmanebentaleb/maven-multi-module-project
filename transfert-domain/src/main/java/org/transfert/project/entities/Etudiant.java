b6e7a79cc6ce433b79c906352b6e87468f181b77
othmanebentaleb <othmanebentaleb90@gmail.com>
2021-10-09 11:16:54 +0200

    [DEV] Ajout des validations sur l'attribut email et téléphone

--- transfert-domain/src/main/java/org/transfert/project/entities/Etudiant.java
+++ transfert-domain/src/main/java/org/transfert/project/entities/Etudiant.java
@@ -6,7 +6,9 @@ import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 
 @Data
 @NoArgsConstructor
@@ -21,7 +23,11 @@ public class Etudiant {
     private String nom;
     @NotEmpty(message = "Prénom ne doit pas être vide")
     private String prenom;
+    @NotEmpty(message = "Numéro de téléphone ne doit pas être vide")
+    @Pattern(regexp="^(?:(?:\\+|00)33[\\s.-]{0,3}(?:\\(0\\)[\\s.-]{0,3})?|0)[1-9](?:(?:[\\s.-]?\\d{2}){4}|\\d{2}(?:[\\s.-]?\\d{3}){2})$",message = "Numéro de téléphone invalide")
     private String telephone;
+    @NotEmpty(message = "Addresse mail ne doit pas être vide")
+    @Email(message = "Veuillez saisir une adresse mail valide")
     private String email;
     @ManyToOne
     @JoinColumn(name = "idClasse")
