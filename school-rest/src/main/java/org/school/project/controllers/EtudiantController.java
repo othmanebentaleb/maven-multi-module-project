02c6421a484ca848897ffef53d004d7776ccb683
othmanebentaleb <othmanebentaleb90@gmail.com>
2021-10-31 15:19:46 +0100

    Suppression des imports inutiles

--- school-rest/src/main/java/org/school/project/controllers/EtudiantController.java
+++ school-rest/src/main/java/org/school/project/controllers/EtudiantController.java
@@ -8,14 +8,12 @@ import org.school.project.services.EtudiantService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
-import org.springframework.http.HttpRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
-import java.util.Locale;
 import java.util.Optional;
 
 @RestController
