08c88e53a2ae8c8ab1141e9ed613704fca6e0337
othmanebentaleb <othmanebentaleb90@gmail.com>
2021-10-31 12:01:43 +0100

    Ajout des validations + Exception sur les paramètres de ka ckasse

--- school-domain/src/main/java/org/school/project/entities/Etudiant.java
+++ school-domain/src/main/java/org/school/project/entities/Etudiant.java
@@ -7,6 +7,9 @@ import lombok.ToString;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
+import java.util.Date;
 
 @Data
 @NoArgsConstructor
@@ -18,9 +21,13 @@ public class Etudiant {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idEtudiant;
     @NotEmpty(message = "Nom ne doit pas être vide")
+    @Size(min = 2, message = "Le nom doit être d'un minimum de 2")
     private String nom;
     @NotEmpty(message = "Prénom ne doit pas être vide")
+    @Size(min = 2, message = "Le prénom doit être d'un minimum de 2")
     private String prenom;
+    @Past
+    private Date dateNaissance;
     @OneToOne(cascade = {CascadeType.ALL}) @JoinColumn(name = "coordonnees")
     private Coordonnees coordonnees;
     @OneToOne(cascade = {CascadeType.ALL}) @JoinColumn(name = "absences")
--- school-rest/src/main/java/org/school/project/exception/CustomizedRespEntityHandler.java
+++ school-rest/src/main/java/org/school/project/exception/CustomizedRespEntityHandler.java
@@ -1,14 +1,18 @@
 package org.school.project.exception;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.Date;
-
+/**
+ *  @author - Othmane BENTALEB
+ */
 @ControllerAdvice
 public class CustomizedRespEntityHandler extends ResponseEntityExceptionHandler {
 
@@ -26,7 +30,13 @@ public class CustomizedRespEntityHandler extends ResponseEntityExceptionHandler 
 
     @ExceptionHandler(EtudiantExistsException.class)
     public final ResponseEntity<Object> handleEtudiantExistsException(EtudiantExistsException ex, WebRequest request) {
-        ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+        ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(true));
         return new ResponseEntity(exceptionResponse, HttpStatus.CONFLICT);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(),ex.getMessage(),ex.getBindingResult().toString());
+        return new ResponseEntity<>(exceptionResponse,HttpStatus.BAD_REQUEST);
     }
 }
