5bf7236ea39813423b28e39b8c3848e3386b8656
othmanebentaleb <othmanebentaleb90@gmail.com>
2021-11-19 20:24:22 +0100

    Affichage du profile activé

--- school-rest/src/main/java/org/school/project/SchoolRestApplication.java
+++ school-rest/src/main/java/org/school/project/SchoolRestApplication.java
@@ -1,11 +1,24 @@
 package org.school.project;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication(scanBasePackages = "org.school.project")
 public class SchoolRestApplication  {
+    @Autowired
+    private Environment environment;
     public static void main(String[] args) {
         SpringApplication.run(SchoolRestApplication.class);
+    }
+    @PostConstruct
+    public void test(){
+        String[] activeProfiles = this.environment.getActiveProfiles();
+        for(String profile:activeProfiles) {
+            System.out.print(profile);
+        }
     }
 }
